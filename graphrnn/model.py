343d9682a0d2b0e7addf584e057d36dc6176cc6a
Skander Moalla <skander@Skanders-MacBook-Pro.local>
2022-03-10 12:16:06 +0000

    First pass over model to update Pytorch API

--- graphrnn/model.py
+++ graphrnn/model.py
@@ -1,6 +1,17 @@
-# plain GRU model
+"""
+Models.
+"""
+from torch import nn
+import torch
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+
+
 class GRU_plain(nn.Module):
-    def __init__(self, input_size, embedding_size, hidden_size, num_layers, has_input=True, has_output=False, output_size=None):
+    """RNN model which can be used for the graph-level RNN and the edge-level RNN"""
+
+    def __init__(
+        self, input_size, embedding_size, hidden_size, num_layers, has_input=True, has_output=False, output_size=None
+    ):
         super(GRU_plain, self).__init__()
         self.num_layers = num_layers
         self.hidden_size = hidden_size
@@ -9,15 +20,14 @@ class GRU_plain(nn.Module):
 
         if has_input:
             self.input = nn.Linear(input_size, embedding_size)
-            self.rnn = nn.GRU(input_size=embedding_size, hidden_size=hidden_size, num_layers=num_layers,
-                              batch_first=True)
+            self.rnn = nn.GRU(
+                input_size=embedding_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True
+            )
         else:
             self.rnn = nn.GRU(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
         if has_output:
             self.output = nn.Sequential(
-                nn.Linear(hidden_size, embedding_size),
-                nn.ReLU(),
-                nn.Linear(embedding_size, output_size)
+                nn.Linear(hidden_size, embedding_size), nn.ReLU(), nn.Linear(embedding_size, output_size)
             )
 
         self.relu = nn.ReLU()
@@ -25,16 +35,16 @@ class GRU_plain(nn.Module):
         self.hidden = None  # need initialize before forward run
 
         for name, param in self.rnn.named_parameters():
-            if 'bias' in name:
+            if "bias" in name:
                 nn.init.constant(param, 0.25)
-            elif 'weight' in name:
-                nn.init.xavier_uniform(param,gain=nn.init.calculate_gain('sigmoid'))
+            elif "weight" in name:
+                nn.init.xavier_uniform(param, gain=nn.init.calculate_gain("sigmoid"))
         for m in self.modules():
             if isinstance(m, nn.Linear):
-                m.weight.data = init.xavier_uniform(m.weight.data, gain=nn.init.calculate_gain('relu'))
+                m.weight.data = nn.init.xavier_uniform(m.weight.data, gain=nn.init.calculate_gain("relu"))
 
     def init_hidden(self, batch_size):
-        return Variable(torch.zeros(self.num_layers, batch_size, self.hidden_size)).cuda()
+        return torch.zeros(self.num_layers, batch_size, self.hidden_size, requires_grad=True)
 
     def forward(self, input_raw, pack=False, input_len=None):
         if self.has_input:
@@ -53,20 +63,17 @@ class GRU_plain(nn.Module):
         return output_raw
 
 
-
 # a deterministic linear output
 class MLP_plain(nn.Module):
     def __init__(self, h_size, embedding_size, y_size):
         super(MLP_plain, self).__init__()
         self.deterministic_output = nn.Sequential(
-            nn.Linear(h_size, embedding_size),
-            nn.ReLU(),
-            nn.Linear(embedding_size, y_size)
+            nn.Linear(h_size, embedding_size), nn.ReLU(), nn.Linear(embedding_size, y_size)
         )
 
         for m in self.modules():
             if isinstance(m, nn.Linear):
-                m.weight.data = init.xavier_uniform(m.weight.data, gain=nn.init.calculate_gain('relu'))
+                m.weight.data = nn.init.xavier_uniform(m.weight.data, gain=nn.init.calculate_gain("relu"))
 
     def forward(self, h):
         y = self.deterministic_output(h)

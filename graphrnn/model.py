4646b3a4b3f8dab5b2e65cd516fe9388d7226b9f
Skander Moalla <skander@Skanders-MacBook-Pro.local>
2022-03-10 12:07:34 +0000

    Copy relevent models.

---
+++ graphrnn/model.py
@@ -0,0 +1,73 @@
+# plain GRU model
+class GRU_plain(nn.Module):
+    def __init__(self, input_size, embedding_size, hidden_size, num_layers, has_input=True, has_output=False, output_size=None):
+        super(GRU_plain, self).__init__()
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.has_input = has_input
+        self.has_output = has_output
+
+        if has_input:
+            self.input = nn.Linear(input_size, embedding_size)
+            self.rnn = nn.GRU(input_size=embedding_size, hidden_size=hidden_size, num_layers=num_layers,
+                              batch_first=True)
+        else:
+            self.rnn = nn.GRU(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
+        if has_output:
+            self.output = nn.Sequential(
+                nn.Linear(hidden_size, embedding_size),
+                nn.ReLU(),
+                nn.Linear(embedding_size, output_size)
+            )
+
+        self.relu = nn.ReLU()
+        # initialize
+        self.hidden = None  # need initialize before forward run
+
+        for name, param in self.rnn.named_parameters():
+            if 'bias' in name:
+                nn.init.constant(param, 0.25)
+            elif 'weight' in name:
+                nn.init.xavier_uniform(param,gain=nn.init.calculate_gain('sigmoid'))
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                m.weight.data = init.xavier_uniform(m.weight.data, gain=nn.init.calculate_gain('relu'))
+
+    def init_hidden(self, batch_size):
+        return Variable(torch.zeros(self.num_layers, batch_size, self.hidden_size)).cuda()
+
+    def forward(self, input_raw, pack=False, input_len=None):
+        if self.has_input:
+            input = self.input(input_raw)
+            input = self.relu(input)
+        else:
+            input = input_raw
+        if pack:
+            input = pack_padded_sequence(input, input_len, batch_first=True)
+        output_raw, self.hidden = self.rnn(input, self.hidden)
+        if pack:
+            output_raw = pad_packed_sequence(output_raw, batch_first=True)[0]
+        if self.has_output:
+            output_raw = self.output(output_raw)
+        # return hidden state at each time step
+        return output_raw
+
+
+
+# a deterministic linear output
+class MLP_plain(nn.Module):
+    def __init__(self, h_size, embedding_size, y_size):
+        super(MLP_plain, self).__init__()
+        self.deterministic_output = nn.Sequential(
+            nn.Linear(h_size, embedding_size),
+            nn.ReLU(),
+            nn.Linear(embedding_size, y_size)
+        )
+
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                m.weight.data = init.xavier_uniform(m.weight.data, gain=nn.init.calculate_gain('relu'))
+
+    def forward(self, h):
+        y = self.deterministic_output(h)
+        return y

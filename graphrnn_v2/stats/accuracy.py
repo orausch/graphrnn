c5821088f093ac93f12792a52a5592ba58d19532
Kyle Duffy <44502053+kyduff@users.noreply.github.com>
2022-04-08 19:03:25 +0100

    Move disconnected condition into calculation
    * if a graph is disconnected, it's incorrect

--- graphrnn_v2/stats/accuracy.py
+++ graphrnn_v2/stats/accuracy.py
@@ -17,7 +17,9 @@ class CycleAccuracy(Accuracy):
     @staticmethod
     def measure(graphs: list[nx.Graph]) -> float:
         # Assume graphs are connected.
-        assert all(nx.is_connected(G) for G in graphs)
-        return sum([all(deg == 2 for _, deg in G.degree()) for G in graphs]) / len(
-            graphs
-        )
+        return sum(
+            [
+                nx.is_connected(G) and all(deg == 2 for _, deg in G.degree())
+                for G in graphs
+            ]
+        ) / len(graphs)

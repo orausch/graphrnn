113889d341b0cc0c98766f45ecf1c750eda8c9db
Skander Moalla <skan.moalla@gmail.com>
2022-04-11 01:28:37 +0200

    Add NLL to eval.

--- graphrnn_v2/experiments/eval.py
+++ graphrnn_v2/experiments/eval.py
@@ -1,10 +1,13 @@
+import networkx as nx
 import torch
+import torch.nn.functional as F
+from torch.nn.utils import rnn as rnnutils
+from torch_geometric.loader import DataLoader
 from tqdm import tqdm
 
 from graphrnn_v2.data import EncodeGraphRNNFeature
-from graphrnn_v2.models import GraphRNN, GraphRNN_S
+from graphrnn_v2.models import GraphRNN
 
-import networkx as nx
 
 def load_model(model_fc, model_path, M, device):
     model = model_fc(M)
@@ -21,3 +24,30 @@ def sample(model, nb_samples, sampler_max_num_nodes, device):
         lengths.append(lens.squeeze(0))
     adjs = EncodeGraphRNNFeature.get_adjacencies_from_sequences(output_sequences, lengths)
     return [nx.from_numpy_array(adj.numpy()) for adj in adjs]
+
+
+def compute_test_nll(model, test_dataset, device, num_workers=0):
+    test_sampler = torch.utils.data.RandomSampler(test_dataset, num_samples=32 * 32, replacement=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=32, num_workers=num_workers, sampler=test_sampler)
+
+    with torch.no_grad():
+        test_nll = 0
+        for batch_idx, batch in enumerate(test_dataloader):
+            batch = batch.to(device)
+
+            lengths = batch.length.cpu()  # torch.split needs a tuple of ints.
+            lengths_tuple = tuple(lengths.tolist())
+            x_padded = rnnutils.pad_sequence(torch.split(batch.x, lengths_tuple), batch_first=True)
+            y_padded = rnnutils.pad_sequence(torch.split(batch.y, lengths_tuple), batch_first=True)
+
+            if isinstance(model, GraphRNN):
+                grouped_edge_seqs = model.get_grouped_edge_sequences_from_y(y_padded, lengths)
+                output_sequences = model(x_padded, lengths, grouped_edge_seqs)
+            else:
+                output_sequences = model(x_padded, lengths)
+
+            test_nll += (
+                    F.binary_cross_entropy(output_sequences.tril(), y_padded, reduction="sum").item()
+                    / batch.num_graphs
+            )
+        return test_nll / len(test_dataloader)
---
+++ graphrnn_v2/experiments/reproduction/ego_small/eval.py
@@ -0,0 +1,23 @@
+import pickle
+
+import torch
+import torch_geometric
+from tqdm import tqdm
+
+from graphrnn_v2.experiments import eval
+from graphrnn_v2.experiments.models import graphrnn_small
+from graphrnn_v2.stats.stats import GraphStats
+
+# GrpahRNN
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+test_dataset = pickle.load(open('./ego_small/test_dataset.pkl', 'rb'))
+test_graphs = [torch_geometric.utils.to_networkx(graph, to_undirected=True) for graph in test_dataset]
+
+for epoch in range(500, 3000, 300):
+    model = eval.load_model(graphrnn_small, f"ego_small/model_at_{epoch}.pt", M=15, device=device)
+    test_nll = eval.compute_test_nll(model, test_dataset, device, num_workers=0)
+    graphs = eval.sample(model, 1024, 20, device)
+    degree = GraphStats.degree(test_graphs, graphs)
+    clustering = GraphStats.clustering(test_graphs, graphs)
+    print(f' {epoch=:.0f}')
+    print(f'{test_nll=:.4f}, {degree=:.4f} {clustering=:.4f}')
